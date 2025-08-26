71c02a1bc4096aee24a5a623454329c4143e66b6
Alexander <alexanderm.kodehode@gmail.com>
2025-08-26 09:59:10 +0200

    const from id

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const pokemonContainer document.querySelecor("#pokemon-container");
 const POKEMON_API_URL = "https://pokeapi.co/api/v2/";
 // Fetch pokemon service, specifying versioning may allow backwards compatability
 const fetchPokemons = async () => {
